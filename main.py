22d4c31e1d4f59587e9aed15ce625dfa87e4f46b
firehoops <tbennettr@gmail.com>
2020-02-28 10:16:07 -0500

    setup

--- main.py
+++ main.py
@@ -1,13 +1,28 @@
+import tkinter as tk
+
 #Text-Based and GUI
 class view():
 
     def __init__(self):
-        return self
+        root = tk.Tk()
+        board = tk.Frame(root)
+        board.pack()
+        canvas = tk.Canvas(root, bg="red", height = 100, width= 200)
+        canvas.pack()
+        root.mainloop()
 
+
+start = view()
 #Handle Connecting view and model
 class controller():
+
+    def __init__(self):
+        return self
 
 
 
 #Handle Logic
 class model():
+
+    def __init__(self):
+        return self
