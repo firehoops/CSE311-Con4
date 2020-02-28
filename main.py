f3a1acb93f5412cf8b1a7467f5be4f438efaa02c
firehoops <tbennettr@gmail.com>
2020-02-28 09:50:56 -0500

    setup

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print("test")
+#Text-Based and GUI
+class view():
+
+    def __init__(self):
+        return self
+
+#Handle Connecting view and model
+class controller():
+
+
+
+#Handle Logic
+class model():
