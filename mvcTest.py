617ec1dc648cdc9b21b0255b19f34091565047ca
Tyler Roop <tbennettr@gmail.com>
2020-03-04 13:01:02 -0500

    updates

--- mvcTest.py
+++ mvcTest.py
@@ -9,7 +9,7 @@ class View:
     def __init__(self,master):
 
         con = Controller()
-        print(con.setBoard())
+        #print(con.setBoard())
 
         option = input("Type text or gui for your version of Connect Four")
 
@@ -34,29 +34,38 @@ class View:
         bottomFrame.grid(row=8, columnspan=7)
         c.grid(row=1, column=0, rowspan=6, columnspan=7)
 
-        rowTracker = [*range(1, 7)]
+        colTracker = [*range(7)]
+        print(colTracker)
         if player1:
-            Button(bottomFrame, text="Add Piece to Row 1", command=lambda: self.addPiece(c, player1, rowTracker[0])).grid(
+            Button(bottomFrame, text="Add Piece to Row 1", command=lambda: self.addPiece(c, player1, colTracker[0])).grid(
                 row=9, column=0, sticky=E)
-            Button(bottomFrame, text="Add Piece to Row 2", command=lambda: self.addPiece(c, player1, rowTracker[1])).grid(
+            Button(bottomFrame, text="Add Piece to Row 2", command=lambda: self.addPiece(c, player1, colTracker[1])).grid(
                 row=9, column=1, sticky=E)
             Button(bottomFrame, text="Exit", command=lambda: self.quit(master)).grid(row=9, column=2, sticky=E)
-            Button(bottomFrame, text="Switch Views", command=lambda: self.switchView(master)).grid(row=9, column=6, sticky=E)
+            Button(bottomFrame, text="Switch Views", command=lambda: self.switchView(master)).grid(row=9, column=3, sticky=E)
 
-    def addPiece(self,canvas, player1, row):
-        x0, y0 = 5, 500
-        x1, y1 = 105, 600
+    def addPiece(self,canvas, player1, col):
+        #Starting coord for row 1 , x0 =5 , y0 = 500 , x1 = 105 , y1= 600
+        coords_col_1 = [5,500,105,600]
+        coords_col_2 = [110, 500, 205, 600]
 
-        if row == 1:
+
+        if col == 0:
             if player1:
-                canvas.create_oval(x0, y0, x1, y1, fill="black")
+                canvas.create_oval(coords_col_1[0], coords_col_1[1], coords_col_1[2], coords_col_1[3], fill="black")
+                coords_col_1[0] += 100
+                coords_col_1[1] += 100
+                coords_col_1[2] += 100
+                coords_col_1[3] += 100
+
+
             else:
-                canvas.create_oval(x0, y0, x1, y1, fill="red")
-        if row == 2:
+                canvas.create_oval(coords_col_1[0], coords_col_1[1], coords_col_1[2], coords_col_1[3], fill="red")
+        if col == 1:
             if player1:
-                canvas.create_oval(x0 + 100, y0 - 100, x1 + 100, y1 - 100, fill="red")
+                canvas.create_oval(coords_col_2[0], coords_col_2[1], coords_col_2[2], coords_col_2[3], fill="black")
             else:
-                canvas.create_oval(x0, y0, x1, y1, fill="red")
+                canvas.create_oval(coords_col_2[0], coords_col_2[1], coords_col_2[2], coords_col_2[3], fill="red")
 
     def switchView(self,master):
         master.destroy()
@@ -66,8 +75,6 @@ class View:
         master.quit()
 
     def textView(self):
-        # contr = Controller()
-        # print(contr().getBoard())
         #Build Board
         ROW_COUNT = 6
         COL_COUNT = 7
@@ -81,10 +88,10 @@ class View:
         #Params: board, row count, col count, user col choice, and
         #Return: Returns nothing prints out the gameboard
         def createBoard(board,ROW_COUNT, COL_COUNT, colChoice, printBoard):
-            boardRot = board[::-1]
+            boardRotate = board[::-1]
             for r in range(ROW_COUNT):
                 for c in range(COL_COUNT):
-                    printBoard += "|" + str(boardRot[r][c])
+                    printBoard += "|" + str(boardRotate[r][c])
                 printBoard += "|\n"
             print(printBoard)
         #Checking if row is free
