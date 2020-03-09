c7f821d8183210b5caa011451449410ec4254031
firehoops <tbennettr@gmail.com>
2020-03-09 10:44:36 -0400

    trying setting canvas as a global variable so can update board as we go

--- mvcTest.py
+++ mvcTest.py
@@ -4,16 +4,13 @@ import tkinter.messagebox
 import time
 import os
 
+
+
 #Text-Based and GUI
 class View:
     def __init__(self):
         self.ROW_COUNT = 6
         self.COL_COUNT = 7
-
-    #Use this to get user text input
-    def getUserInput(self, message):
-        userInput = input(message)
-        return userInput
 
     #Params: board, row count, col count, user col choice, and
     #Return: Returns nothing prints out the gameboard
@@ -69,10 +66,11 @@ class Model:
         for rowNum in range(ROW_COUNT):
             if board[rowNum][colChoice] == 0:
                 return rowNum
+    #def addToBoard(self, board,colChoice):
 
     def makeMove(self, colChoice):
-        row = model.checkRow(model.getBoard(), colChoice, self.ROW_COUNT)
-        model.getBoard()[row][colChoice] = self.playerValue
+        row = self.checkRow(model.getBoard(), colChoice, self.ROW_COUNT)
+        self.getBoard()[row][colChoice] = self.playerValue
         self.moveCount += 1
 
         if model.winnerExists():
@@ -103,13 +101,13 @@ class Controller:
         self.coords_col_6 = [500, 500, 600, 600]
         self.coords_col_7 = [600, 500, 700, 600]
         #Starting the game
-        option = view.getUserInput("Type text or gui for your version of Connect Four\n")
+        option = self.getUserInput("Type text or gui for your version of Connect Four\n")
         if option.lower() == "gui":
-            self.gui(master)
+            self.gui(master,c)
         else:
-            self.textView(master)
+            self.textView(master,c)
 
-    def textView(self,master):
+    def textView(self,master,c):
         run = True
 
         #Game Loop
@@ -123,14 +121,15 @@ class Controller:
                     view.displayBoard(model.getBoard())
 
                     colChoice = int(
-                        view.getUserInput("Which Column 0,1,2,3,4,5, or 6 (or type 9 to go into gui view) \n"))
+                        model.getUserInput("Which Column 0,1,2,3,4,5, or 6 (or type 9 to go into gui view) \n"))
                     if colChoice == 8:
-                        break #Continues game
+                        run = False
+                        return False #Continues game
                     if colChoice == 9:
-                        print("made it to 9")
+                        model.gui(master,c)
 
-                        break
-                    run = model.makeMove(colChoice)
+                    #self.addPiece(colChoice)
+                    run = model.addPiece(colChoice)
 
                     break#Exits the User Input Loop
                 except:
@@ -138,10 +137,19 @@ class Controller:
                     time.sleep(.5)
         exit()
 
+        # ***Controller?
+        # Use this to get user text input
+    def getUserInput(self, message):
+        userInput = input(message)
+        return userInput
+
+
+
+
     #Creates the gui
     #Params: master is the main root
     #Returns: Creates a gui
-    def gui(self,master):
+    def gui(self,master,c):
 
         header = tkinter.font.Font(size=20, weight=tkinter.font.BOLD)
         Label(master, text="Connect Four", anchor=N, font=header).grid(row=0, column=2, columnspan=2)
@@ -151,7 +159,6 @@ class Controller:
         run = True
 
         # Main Canvas that board will be created and played on
-        c = Canvas(master, width=700, height=600, bg="gray")
 
         # Create Seperation from Board to have buttons and exit/switch view buttons
         bottomFrame = Frame(master, width=700, height=200)
@@ -181,62 +188,62 @@ class Controller:
     #Adds a piece to the board
     #Params:Canvas, Boolean player value, col which was selected
     #Returns: Creates a piece on the board
-    def addPiece(self,canvas, player1, col):
+    def addPiece(self,canvas, player1, colChoice):
         #Starting coord for row 1 , x0 =5 , y0 = 500 , x1 = 105 , y1= 600
 
 
-        if col == 0:
+        if colChoice == 0:
             if player1:
                 canvas.create_oval(self.coords_col_1[0], self.coords_col_1[1], self.coords_col_1[2], self.coords_col_1[3], fill="black")
-                #self.coords_col_1[0] -= 100
+
                 self.coords_col_1[1] -= 100
-                #self.coords_col_1[2] -= 100
                 self.coords_col_1[3] -= 100
-                model.makeMove(0)
+                model.makeMove(colChoice)
+
 
             else:
                 canvas.create_oval(self.coords_col_1[0], self.coords_col_1[1], self.coords_col_1[2], self.coords_col_1[3], fill="red")
-        if col == 1:
+        if colChoice == 1:
             if player1:
                 canvas.create_oval(self.coords_col_2[0], self.coords_col_2[1], self.coords_col_2[2], self.coords_col_2[3], fill="black")
                 # self.coords_col_1[0] -= 100
                 self.coords_col_2[1] -= 100
                 # self.coords_col_1[2] -= 100
                 self.coords_col_2[3] -= 100
-                model.makeMove(1)
+                model.makeMove(colChoice)
             else:
                 canvas.create_oval(self.coords_col_2[0], self.coords_col_2[1], self.coords_col_2[2], self.coords_col_2[3], fill="red")
-        if col == 2:
+        if colChoice == 2:
             if player1:
                 canvas.create_oval(self.coords_col_3[0], self.coords_col_3[1], self.coords_col_3[2], self.coords_col_3[3], fill="black")
                 # self.coords_col_1[0] -= 100
                 self.coords_col_3[1] -= 100
                 # self.coords_col_1[2] -= 100
                 self.coords_col_3[3] -= 100
-                model.makeMove(2)
+                model.makeMove(colChoice)
             else:
                 canvas.create_oval(self.coords_col_3[0], self.coords_col_3[1], self.coords_col_3[2], self.coords_col_3[3], fill="red")
-        if col == 3:
+        if colChoice == 3:
             if player1:
                 canvas.create_oval(self.coords_col_4[0], self.coords_col_4[1], self.coords_col_4[2], self.coords_col_4[3], fill="black")
                 # self.coords_col_1[0] -= 100
                 self.coords_col_4[1] -= 100
                 # self.coords_col_1[2] -= 100
                 self.coords_col_4[3] -= 100
-                model.makeMove(3)
+                model.makeMove(colChoice)
             else:
                 canvas.create_oval(self.coords_col_4[0], self.coords_col_4[1], self.coords_col_4[2], self.coords_col_4[3], fill="red")
-        if col == 4:
+        if colChoice == 4:
             if player1:
                 canvas.create_oval(self.coords_col_5[0], self.coords_col_5[1], self.coords_col_5[2], self.coords_col_5[3], fill="black")
                 # self.coords_col_1[0] -= 100
                 self.coords_col_5[1] -= 100
                 # self.coords_col_1[2] -= 100
                 self.coords_col_5[3] -= 100
-                model.makeMove(4)
+                model.makeMove(colChoice)
             else:
                 canvas.create_oval(self.coords_col_5[0], self.coords_col_5[1], self.coords_col_5[2], self.coords_col_5[3], fill="red")
-        if col == 5:
+        if colChoice == 5:
             if player1:
                 canvas.create_oval(self.coords_col_6[0], self.coords_col_6[1], self.coords_col_6[2],
                                    self.coords_col_6[3], fill="black")
@@ -244,18 +251,18 @@ class Controller:
                 self.coords_col_6[1] -= 100
                 # self.coords_col_1[2] -= 100
                 self.coords_col_6[3] -= 100
-                model.makeMove(5)
+                model.makeMove(colChoice)
             else:
                 canvas.create_oval(self.coords_col_6[0], self.coords_col_6[1], self.coords_col_6[2],
                                    self.coords_col_6[3], fill="red")
-        if col == 6:
+        if colChoice == 6:
             if player1:
                 canvas.create_oval(self.coords_col_7[0], self.coords_col_7[1], self.coords_col_7[2], self.coords_col_7[3], fill="black")
                 # self.coords_col_1[0] -= 100
                 self.coords_col_7[1] -= 100
                 # self.coords_col_1[2] -= 100
                 self.coords_col_7[3] -= 100
-                model.makeMove(6)
+                model.makeMove(colChoice)
             else:
                 canvas.create_oval(self.coords_col_7[0], self.coords_col_7[1], self.coords_col_7[2], self.coords_col_7[3], fill="red")
 
@@ -276,7 +283,7 @@ class Controller:
     #Returns: Creates the text view board
 
 #Starts Tkinter window and runs in loop until user exits
-
 root = Tk()
 start = Controller(root)
 root.mainloop()
+c = Canvas(root, width=700, height=600, bg="gray")
